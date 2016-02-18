9037f2ec3838f87476e00ab43f313bcaa1e5f3e2
klestlecat <klest.lecat@gmail.com>
2016-02-18 17:36:36 +0100

    commit
    creation of Arraylists to store messages, greation of the first POST API

---
+++ src/main/java/org/klest/javatutorial/messenger/database/DatabaseClass.java
@@ -0,0 +1,25 @@
+package org.klest.javatutorial.messenger.database;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.klest.javatutorial.messenger.model.Message;
+import org.klest.javatutorial.messenger.model.Profile;
+
+public class DatabaseClass {
+
+	// This is not thread safe he said??? (No concurrency protection in the following lines
+	//It is just for the purposes of the tutorial
+	private static Map<Long, Message> messages = new HashMap<>();
+	private static Map<Long, Profile> profiles = new HashMap<>();
+
+	
+	public static Map<Long, Message> getMessages() {
+		return messages;
+	}
+	
+	public static Map<Long, Profile> getProfiles() {
+		return profiles;
+	}
+	
+}
--- src/main/java/org/klest/javatutorial/messenger/resources/MessageResource.java
+++ src/main/java/org/klest/javatutorial/messenger/resources/MessageResource.java
@@ -2,8 +2,11 @@ package org.klest.javatutorial.messenger.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -16,9 +19,29 @@ public class MessageResource {
 	MessageService messageService = new MessageService();
 	
 	@GET
-	@Produces(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_JSON)
 	public List<Message> getMessages(){
 		return messageService.getAllMessages();
+		
+	}
+	
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message addMessage(Message message){
+		
+		return messageService.addMessage(message);
+	}
+	
+	
+	@GET
+	@Path("/{messageId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message test(@PathParam("messageId") long messageId){
+		
+		return messageService.getMessage(messageId);
+		
 		
 	}
 }
