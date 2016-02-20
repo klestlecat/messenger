af6fb12da3b1c17f56cede91329f8c0817ca8b4c
klestlecat <klest.lecat@gmail.com>
2016-02-20 14:22:47 +0100

    Commit debut de seance
    PUT method created in MessageRessource class

--- src/main/java/org/klest/javatutorial/messenger/resources/MessageResource.java
+++ src/main/java/org/klest/javatutorial/messenger/resources/MessageResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -34,6 +35,12 @@ public class MessageResource {
 		return messageService.addMessage(message);
 	}
 	
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message updateMessage( Message message){
+		return messageService.updateMessage(message);
+	}
 	
 	@GET
 	@Path("/{messageId}")
