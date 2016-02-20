f7c6d6e7319de97107baabc67ae7b06e51b5ccbb
klestlecat <klest.lecat@gmail.com>
2016-02-20 15:24:27 +0100

    commit Profile ressource
    Profile ressource complete

--- src/main/java/org/klest/javatutorial/messenger/service/ProfileService.java
+++ src/main/java/org/klest/javatutorial/messenger/service/ProfileService.java
@@ -1,5 +1,45 @@
 package org.klest.javatutorial.messenger.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.klest.javatutorial.messenger.database.DatabaseClass;
+import org.klest.javatutorial.messenger.model.Profile;
+
 public class ProfileService {
 
+private Map<String, Profile> profiles = DatabaseClass.getProfiles();
+	
+	public ProfileService() {
+		profiles.put("klest", new Profile(1L, "klest", "Klest", "lecat"));
+	}
+	
+	public List<Profile> getAllProfiles() {
+		return new ArrayList<Profile>(profiles.values()); 
+	}
+	
+	public Profile getProfile(String profileName) {
+		return profiles.get(profileName);
+	}
+	
+	public Profile addProfile(Profile profile) {
+		profile.setId(profiles.size() + 1);
+		profiles.put(profile.getProfileName(), profile);
+		return profile;
+	}
+	
+	public Profile updateProfile(Profile profile) {
+		if (profile.getProfileName().isEmpty()) {
+			return null;
+		}
+		profiles.put(profile.getProfileName(), profile);
+		return profile;
+	}
+	
+	public Profile removeProfile(String profileName) {
+		return profiles.remove(profileName);
+	}
+	
+	
 }
